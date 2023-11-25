a94727e92ccaf6988f64fcdfa7f7aebbb8acd8dc
Code With Naim <111947358+CodeWithNaim@users.noreply.github.com>
2023-11-25 14:32:26 +0600

    added todo app

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Todos from "./components/Todo_App/Todos";
+
+function App() {
+  return (
+    <>
+      <Todos />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Todo_App/Todo.jsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+const Todo = (todos, setTodos) => {
+
+
+  return (
+    
+  );
+};
+
+export default Todo;
